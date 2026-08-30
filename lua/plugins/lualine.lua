8d22e1b3c1fe3ad4cfc2b32f16290ffafda4b00f
DZZc0rd <lenovoigoria@gmail.com>
2026-08-30 12:42:26 +0300

    configure lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,1 +1,33 @@
-require("lualine").setup()
+local function file_ext()
+    local ft = vim.o.filetype
+
+    if ft ~= "alpha" then
+        return MiniIcons.get("extension", vim.o.filetype) .. " " .. vim.o.filetype
+    end
+    return "NONE"
+end
+
+require("lualine").setup({
+    options = {
+        component_separators = "",
+        section_separators = "",
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = {},
+        lualine_c = { "branch", "diff", "diagnostics" },
+
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = { file_ext },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+    },
+})
