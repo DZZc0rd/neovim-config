2195d55f181a3cf47d3b10515ea3e6d868b060b0
DZZc0rd <lenovoigoria@gmail.com>
2026-09-05 19:44:52 +0300

    add some keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,18 @@
-local keymap = vim.keymap
+local map = vim.keymap.set
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-keymap.set("n", "<leader>f", "<cmd>FzfLua files<cr>", { desc = "Find file" })
-keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<cr>", { desc = "Find buffers" })
-keymap.set("n", "<leader>t", "<cmd>FzfLua live_grep<cr>", { desc = "Find text" })
+map("n", "<leader>f", "<cmd>FzfLua files<cr>", { desc = "Find file" })
+map("n", "<leader>b", "<cmd>FzfLua buffers<cr>", { desc = "Find buffers" })
+map("n", "<leader>t", "<cmd>FzfLua live_grep<cr>", { desc = "Find text" })
 
-keymap.set("n", "<leader>x", "<cmd>FzfLua diagnostics_workspace<cr>", { desc = "Diagnostics" })
+map("n", "<leader>x", "<cmd>FzfLua diagnostics_workspace<cr>", { desc = "Diagnostics" })
+
+map({ "n", "v", "i" }, "<c-h>", "<c-w>h", { desc = "Go to left window" })
+map({ "n", "v", "i" }, "<c-j>", "<c-w>j", { desc = "Go to lower window" })
+map({ "n", "v", "i" }, "<c-k>", "<c-w>k", { desc = "Go to upper window" })
+map({ "n", "v", "i" }, "<c-l>", "<c-w>l", { desc = "Go to right window" })
+
+map("n", "<leader>wh", "<c-w>s", { desc = "Split window horizontally" })
+map("n", "<leader>wv", "<c-w>v", { desc = "Split window vertically" })
