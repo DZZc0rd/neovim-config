21e732a527802c89094cce069239c5b614a78510
DZZc0rd <lenovoigoria@gmail.com>
2026-09-05 20:11:19 +0300

    make matugen module optional

--- lua/plugins/matugen.lua
+++ lua/plugins/matugen.lua
@@ -1,4 +1,13 @@
-require("matugen").setup({
+local success, matugen = pcall(require, "matugen")
+
+if not success then
+    vim.notify("`matugen.nvim` is not installed. Using `catppuccin` instead", vim.log.levels.WARN)
+    vim.cmd("colorscheme catppuccin")
+
+    return
+end
+
+matugen.setup({
     palette_path = "~/.config/matugen-theme/nvim.json",
     load_theme = true,
 })
